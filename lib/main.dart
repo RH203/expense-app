33c04ffe592b2b1edc037fda53c824048e4989fc
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-08 15:08:57 +0700

    feat: Add ExpenseStore to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:expense_app/src/app.dart';
+import 'package:expense_app/src/store/expense_store.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ExpenseStore())],
+      child: const App(),
+    ),
+  );
 }
-
